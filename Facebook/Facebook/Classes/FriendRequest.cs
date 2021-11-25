d21eaf128ac7abc181d35d1523e5827de9fd3c3a
Tommy Harvey <1531915@cstj.qc.ca>
2021-11-25 18:38:00 -0500

    Mostly finished nav, added most icons for future pages

---
+++ Facebook/Facebook/Classes/FriendRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facebook.Classes
+{
+	class FriendRequest
+	{
+		private User _sender;
+		private User _receiver;
+
+		public FriendRequest(User sender, User receiver)
+		{
+			_sender = sender;
+			_receiver = receiver;
+		}
+	}
+}
---
+++ Facebook/Facebook/Classes/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facebook.Classes
+{
+	class User
+	{
+		private string _firstName;
+		private string _lastName;
+		private string _password;
+		private string _email;
+
+		private Uri _profileImage;
+		private Uri _profileBanner;
+
+		private List<User> _addedFriends = new List<User>();
+	}
+}
