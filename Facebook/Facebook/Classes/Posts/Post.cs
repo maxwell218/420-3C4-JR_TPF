4ddec310f5d9d598dc0ffc38afa8b48bd5e373c2
Tommy Harvey <1531915@cstj.qc.ca>
2021-12-17 12:21:56 -0500

    Transferring work from laptop to desktop

--- Facebook/Facebook/Classes/Posts/Post.cs
+++ Facebook/Facebook/Classes/Posts/Post.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
 
 namespace Facebook
 {
 	public class Post
 	{
+		private const int POST_ROWS = 7;
 		public User Poster;
 		public int Score = 0;
 		public DateTime? Date;
@@ -42,5 +53,30 @@ namespace Facebook
 			Date = (date == null) ? DateTime.Now : date;
 			MediaContent = media;
 		}
+
+		public ListViewItem CreatePostXAML()
+        {
+			ListViewItem post = new ListViewItem();
+
+			// Grid that formats all the content of the post
+			Grid grid = new Grid();
+			grid.ColumnDefinitions.Add(new ColumnDefinition());
+			grid.ColumnDefinitions.Add(new ColumnDefinition());
+			grid.ColumnDefinitions[0].Width = GridLength.Auto;
+			grid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Star);
+
+			for (int i = 0; i < POST_ROWS; i++)
+            {
+				grid.RowDefinitions.Add(new RowDefinition());
+            }
+
+			// User image
+			Border border = new Border();
+			border.Style = (Style) App.Current.Resources["PostUserImageStyle"];
+			ImageBrush userImage = new ImageBrush(new BitmapImage(Poster.ProfileImage));
+			border.Background = userImage;
+
+			return post;
+        }
 	}
 }
